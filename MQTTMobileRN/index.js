0f6a5588d2892ae06c45c9e4a66c3145e3eac9c2
Juan De la Cruz <juan.carlos.cc@outlook.com>
2019-03-19 16:27:41 -0600

    Add react native paper

--- MQTTMobileRN/index.js
+++ MQTTMobileRN/index.js
@@ -1,9 +1,13 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => (
+  <PaperProvider>
+    <App />
+  </PaperProvider>
+);
+
+AppRegistry.registerComponent(appName, () => Main);

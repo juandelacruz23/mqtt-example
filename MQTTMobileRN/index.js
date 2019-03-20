e9fb717d669b971d2f39693ee7252231d739b354
Juan De la Cruz <juan.carlos.cc@outlook.com>
2019-03-20 13:23:26 -0600

    Move al react app files to its own folder

--- MQTTMobileRN/index.js
+++ MQTTMobileRN/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import App from './App';
+import App from './src/App';
 import { name as appName } from './app.json';
 
 const Main = () => (

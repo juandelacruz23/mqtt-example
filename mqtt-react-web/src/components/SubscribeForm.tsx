7b7f1a0b224a84a93b9db13c3f9cf254edfdbc29
Juan De la Cruz <juan.carlos.cc@outlook.com>
2019-12-06 22:30:45 -0600

    Implement basic Formik

--- mqtt-react-web/src/components/SubscribeForm.tsx
+++ mqtt-react-web/src/components/SubscribeForm.tsx
@@ -4,9 +4,14 @@ import {
   Label,
   TextField,
   IComboBoxOption,
-  ComboBox,
   DefaultButton,
 } from "office-ui-fabric-react";
+import { Form, Formik, FormikProps } from "formik";
+
+const defaultSubscriptionValues = {
+  topic: "message",
+  qos: 0,
+};
 
 const QoSOptions: IComboBoxOption[] = [
   { key: 0, text: "0" },
@@ -14,29 +19,61 @@ const QoSOptions: IComboBoxOption[] = [
   { key: 2, text: "2" },
 ];
 
+const FormInternal = (
+  formik: FormikProps<{ qos: number; topic: string }>,
+): JSX.Element => {
+  const disabled = formik.values.qos == null || !formik.values.topic;
+  return (
+    <Form>
+      <Stack
+        tokens={{ childrenGap: 10 }}
+        styles={{ root: { textAlign: "start" } }}
+      >
+        <Stack horizontal>
+          <Label className="subscribe-label">Topic</Label>
+          <TextField
+            {...formik.getFieldProps("topic")}
+            className="fill-space"
+          />
+        </Stack>
+        <Stack horizontal>
+          <Label className="subscribe-label">QoS</Label>
+          <select {...formik.getFieldProps("qos")} className="fill-space">
+            {QoSOptions.map(qosOption => (
+              <option
+                key={`subscribe-qos-${qosOption.key}`}
+                value={qosOption.key}
+              >
+                {qosOption.text}
+              </option>
+            ))}
+          </select>
+        </Stack>
+        <Stack
+          className="subscribe-buttons"
+          tokens={{ childrenGap: 5 }}
+          horizontal
+        >
+          <DefaultButton type="submit" text="Subscribe" disabled={disabled} />
+        </Stack>
+      </Stack>
+    </Form>
+  );
+};
+
 const SubscribeForm: FC = () => {
   return (
-    <Stack
-      tokens={{ childrenGap: 10 }}
-      styles={{ root: { textAlign: "start" } }}
+    <Formik
+      initialValues={defaultSubscriptionValues}
+      onSubmit={(values, { setSubmitting }): void => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
     >
-      <Stack horizontal>
-        <Label className="subscribe-label">Topic</Label>
-        <TextField className="fill-space" defaultValue="message" />
-      </Stack>
-      <Stack horizontal>
-        <Label className="subscribe-label">QoS</Label>
-        <ComboBox className="fill-space" options={QoSOptions} selectedKey={0} />
-      </Stack>
-      <Stack
-        className="subscribe-buttons"
-        tokens={{ childrenGap: 5 }}
-        horizontal
-      >
-        <DefaultButton text="Subscribe" />
-        <DefaultButton text="Unsubscribe" />
-      </Stack>
-    </Stack>
+      {FormInternal}
+    </Formik>
   );
 };
 
